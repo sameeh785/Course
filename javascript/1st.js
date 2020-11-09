f0806c113b37d50a991c4b0f546f4575da025592
sami <sameeh785@gmail.com>
2020-11-08 20:42:54 -0800

    'updated'

---
+++ javascript/1st.js
@@ -0,0 +1,14 @@
+let marks = 33;
+
+let attendence = false;
+
+if( marks > 80){
+
+    alert('Muna pass ho gia')
+}
+else if( attendence == true){
+    alert('muna attendece ki base pr pass hova')
+}
+else{
+    alert('muna fail ho gia')
+}
